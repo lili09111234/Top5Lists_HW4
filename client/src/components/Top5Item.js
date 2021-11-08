85b60fc83774698c14dbf8aca3b1270764704deb
hongcheng li <lili09111234@gmail.com>
2021-11-08 14:03:51 -0500

    EDIT ITEM WORK UNDO REDO ALSO WORK

--- client/src/components/Top5Item.js
+++ client/src/components/Top5Item.js
@@ -15,6 +15,7 @@ function Top5Item(props) {
     const { store } = useContext(GlobalStoreContext);
     const [editActive, setEditActive] = useState(false);
     const [draggedTo, setDraggedTo] = useState(0);
+    const [ text, setText ] = useState(props.text);
 
     function handleDragStart(event, targetId) {
         event.dataTransfer.setData("item", targetId);
@@ -48,6 +49,37 @@ function Top5Item(props) {
         store.addMoveItemTransaction(sourceId, targetId);
     }
 
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            store.itemActive=false;
+            let target = event.target;
+            let targetId = target.id;
+            console.log(targetId);
+            targetId = targetId.charAt(10);
+            console.log(targetId-1);
+            console.log(store.currentList.items[targetId-1]);
+            console.log(text);
+            store.addUpdateItemTransaction(targetId-1,text);
+            toggleEdit();
+        }
+    }
+
+    function toggleEdit() {
+        let newActive = !editActive;
+        if (newActive) {  
+            store.setIsItemEditActive();
+        }
+        setEditActive(newActive);
+    }
+    function handleToggleEdit(event) {
+        event.stopPropagation();
+        toggleEdit();
+    }
+
+    function handleUpdateText(event) {
+        setText(event.target.value);
+    }
+
     let { index } = props;
 
     let itemClass = "top5-item";
@@ -55,8 +87,9 @@ function Top5Item(props) {
         itemClass = "top5-item-dragged-to";
     }
 
-    return (
-            <ListItem
+    if(!editActive){
+        var re=(
+        <ListItem
                 id={'item-' + (index+1)}
                 key={props.key}
                 className={itemClass}
@@ -83,13 +116,35 @@ function Top5Item(props) {
                 }}
             >
             <Box sx={{ p: 1 }}>
-                <IconButton aria-label='edit'>
+                <IconButton onClick={handleToggleEdit} aria-label='edit'>
                     <EditIcon style={{fontSize:'48pt'}}  />
                 </IconButton>
             </Box>
                 <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
             </ListItem>
-    )
+        )
+    }
+    else{
+        re=( <TextField
+            margin="normal"
+            required
+            fullWidth
+            id={"edit-item-" + (index+1) }
+            label="Top 5 Item Name"
+            name="name"
+            autoComplete="Top 5 Item Name"
+            className='list-card'
+            onKeyPress={handleKeyPress}
+            onChange={handleUpdateText}
+            defaultValue={props.text}
+            inputProps={{style: {fontSize: 48}}}
+            InputLabelProps={{style: {fontSize: 24}}}
+            autoFocus
+        />
+        )
+    }
+
+    return re
 }
 
 export default Top5Item;
