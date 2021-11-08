5c1e49779935d9e6d83ba3905475dd9d40baf7b7
hongcheng li <lili09111234@gmail.com>
2021-11-08 11:03:14 -0500

    limited edit/delet/load

--- server/controllers/top5list-controller.js
+++ server/controllers/top5list-controller.js
@@ -35,67 +35,120 @@ createTop5List = (req, res) => {
 }
 
 updateTop5List = async (req, res) => {
-    const body = req.body
-    console.log("updateTop5List: " + JSON.stringify(body));
-    if (!body) {
-        return res.status(400).json({
-            success: false,
-            error: 'You must provide a body to update',
-        })
-    }
+    try{
+        const token = req.cookies.token;
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const resuser = await users.findById({_id: verified.userId}).lean();
+        if(resuser){
+            const body = req.body
+            console.log("updateTop5List: " + JSON.stringify(body));
+            if (!body) {
+                return res.status(400).json({
+                    success: false,
+                    error: 'You must provide a body to update',
+                })
+            }
 
-    Top5List.findOne({ _id: req.params.id }, (err, top5List) => {
-        console.log("top5List found: " + JSON.stringify(top5List));
-        if (err) {
-            return res.status(404).json({
-                err,
-                message: 'Top 5 List not found!',
+            Top5List.findOne({ _id: req.params.id }, (err, top5List) => {
+                console.log("top5List found: " + JSON.stringify(top5List));
+                if (err) {
+                    return res.status(404).json({
+                        err,
+                        message: 'Top 5 List not found!',
+                    })
+                }
+                if (top5List.ownerEmail==resuser.email){
+                    top5List.name = body.name
+                    top5List.items = body.items
+                    top5List
+                        .save()
+                        .then(() => {
+                            console.log("SUCCESS!!!");
+                            return res.status(200).json({
+                                success: true,
+                                id: top5List._id,
+                                message: 'Top 5 List updated!',
+                            })
+                        })
+                        .catch(error => {
+                            console.log("FAILURE: " + JSON.stringify(error));
+                            return res.status(404).json({
+                                error,
+                                message: 'Top 5 List not updated!',
+                            })
+                        })
+                }
+                else{
+                    return res.status(403).json({ success: false, error: err });
+                }
             })
         }
-
-        top5List.name = body.name
-        top5List.items = body.items
-        top5List
-            .save()
-            .then(() => {
-                console.log("SUCCESS!!!");
-                return res.status(200).json({
-                    success: true,
-                    id: top5List._id,
-                    message: 'Top 5 List updated!',
-                })
-            })
-            .catch(error => {
-                console.log("FAILURE: " + JSON.stringify(error));
-                return res.status(404).json({
-                    error,
-                    message: 'Top 5 List not updated!',
-                })
-            })
-    })
+        else{
+            return res.status(400).json({ success: false, error: err });
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 
 deleteTop5List = async (req, res) => {
-    Top5List.findById({ _id: req.params.id }, (err, top5List) => {
-        if (err) {
-            return res.status(404).json({
-                err,
-                message: 'Top 5 List not found!',
+    try{
+        const token = req.cookies.token;
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const resuser = await users.findById({_id: verified.userId}).lean();
+        if(resuser){
+            Top5List.findById({ _id: req.params.id }, (err, top5List) => {
+                if (err) {
+                    return res.status(404).json({
+                        err,
+                        message: 'Top 5 List not found!',
+                    })
+                }
+                if(top5List.ownerEmail==resuser.email){
+                    Top5List.findOneAndDelete({ _id: req.params.id }, () => {
+                        return res.status(200).json({ success: true, data: top5List })
+                    }).catch(err => console.log(err))
+                }
+                else{
+                    return res.status(403).json({ success: false, error: err });
+                }
             })
         }
-        Top5List.findOneAndDelete({ _id: req.params.id }, () => {
-            return res.status(200).json({ success: true, data: top5List })
-        }).catch(err => console.log(err))
-    })
+        else{
+            return res.status(400).json({ success: false, error: err });
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 
 getTop5ListById = async (req, res) => {
-    await Top5List.findById({ _id: req.params.id }, (err, list) => {
-        if (err) {
+    try{
+        const token = req.cookies.token;
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const resuser = await users.findById({_id: verified.userId}).lean();
+        if(resuser){
+        await Top5List.findById({ _id: req.params.id }, (err, list) => {
+            if (err) {
+                return res.status(400).json({ success: false, error: err });
+            }
+            if(list.ownerEmail==resuser.email){
+            return res.status(200).json({ success: true, top5List: list })
+            }
+            else{
+                return res.status(403).json({ success: false, error: err });
+            }
+        }).catch(err => console.log(err))
+        }
+        else{
             return res.status(400).json({ success: false, error: err });
         }
-        return res.status(200).json({ success: true, top5List: list })
-    }).catch(err => console.log(err))
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 getTop5Lists = async (req, res) => {
     await Top5List.find({}, (err, top5Lists) => {
@@ -111,14 +164,13 @@ getTop5Lists = async (req, res) => {
     }).catch(err => console.log(err))
 }
 getTop5ListPairs = async (req, res) => {
-        try {
-            const token = req.cookies.token;
-            const verified = jwt.verify(token, process.env.JWT_SECRET);
-            const resuser = await users.findById({_id: verified.userId}).lean();
-            console.log(resuser.email);
+            try {
+                const token = req.cookies.token;
+                const verified = jwt.verify(token, process.env.JWT_SECRET);
+                const resuser = await users.findById({_id: verified.userId}).lean();
          
     await Top5List.find({ownerEmail: resuser.email  }, (err, top5Lists) => {
-        if (err) {
+        if (err) {  
             return res.status(400).json({ success: false, error: err })
         }
         if (!top5Lists) {
